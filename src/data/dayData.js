973e3252f5037e39c548e3b7a3d95b8424e40321
bevanj1 <bevanjacinto@gmail.com>
2025-05-02 00:01:25 +0530

    Day 2

--- src/data/dayData.js
+++ src/data/dayData.js
@@ -5,12 +5,17 @@ const dayData = [
       "To my 😊Adorable😊 little kitten. I have no idea how I bagged this walking talking ball of cuteness. Please always stay my sweet little angel so that you can be the most Adorable person I know. Im so excited to celebrate your birthday with you. Have the most amazing day",
     media: ["img1.jpeg", "img2.jpeg", "img3.jpeg", "vid1.mp4", "vid2.mp4"],
   },
-  //   {
+  {
+    day: 2,
+    message:
+      "Each you you keep amazing me how 🌹Beautiful🌹 you are. I still thing of all the times you've looked absolutely drop dead gorgeous and I cant believe you're mine. I still think of Goa and Maroon 5 and all the timmes you just keep getting prettier everyday and aging like fine wine. I love you baby. Thank you so much for exisitng. One more day down to your bday im so excited.",
+    media: ["img1.jpeg", "img2.jpeg", "img3.jpeg", "vid1.mp4", "vid2.mp4"],
+  },
+  // {
   //     day: 2,
   //     message: "Day 2 is here with more surprises! 🎉",
   //     media: ["pic2.jpg", "clip2.mp4"],
   //   },
-  // Add more daily entries as you go
 ];
 
 export default dayData;
