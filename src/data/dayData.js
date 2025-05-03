99d0ba46479583d6c6ab732a8ea80795a110eef2
bevanj1 <bevanjacinto@gmail.com>
2025-05-03 12:20:40 +0530

    Day 3

--- src/data/dayData.js
+++ src/data/dayData.js
@@ -8,8 +8,14 @@ const dayData = [
   {
     day: 2,
     message:
-      "Each you you keep amazing me how 🌹Beautiful🌹 you are. I still thing of all the times you've looked absolutely drop dead gorgeous and I cant believe you're mine. I still think of Goa and Maroon 5 and all the timmes you just keep getting prettier everyday and aging like fine wine. I love you baby. Thank you so much for exisitng. One more day down to your bday im so excited.",
+      "Each day you keep amazing me how 🌹Beautiful🌹 you are. I still think of all the times you've looked absolutely drop dead gorgeous and I cant believe you're mine. I still think of Goa and Maroon 5 and all the times you just keep getting prettier everyday and aging like fine wine. I love you baby. Thank you so much for exisitng. One more day down to your bday im so excited.",
     media: ["img1.jpeg", "img2.jpeg", "img3.jpeg", "vid1.mp4", "vid2.mp4"],
+  },
+  {
+    day: 3,
+    message:
+      "My 🥰Cute🥰 little baby. I smile everytime I look at your photo. I always imagined and prayed to God for a woman like you and you just know how to make my heart melt. I love you to the moon and back. One more day down wohooo!!",
+    media: ["img1.jpeg", "img2.jpeg", "img3.jpeg", "img4.jpeg", "vid1.mp4"],
   },
   // {
   //     day: 2,
